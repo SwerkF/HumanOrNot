1b3b6569533216fd749663c6d6666d850f0ee66c
swerk <swerk.pro@gmail.com>
2025-02-01 19:55:49 +0100

    Rendu final projet

--- backend/server.js
+++ backend/server.js
@@ -5,223 +5,239 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoute");
 const gameRoute = require("./routes/gameRoute");
 const ws = require("ws");
-const wss = new ws.Server({ port: 8080 });
 const { default: ollama } = require("ollama");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const Game = require("./models/Game");
+const Message = require("./models/Message");
 
-const Game = require('./models/Game');
-const Message = require('./models/Message');
-const Player = require('./models/Player');
+dotenv.config();
 
+const wss = new ws.Server({ port: process.env.PORT_WS });
 const games = []; // randomId, wsUserOne, wsUserTwo or null of against bot, messages
 let waitingPlayer = null;
 
-const randomIdGenerator = () => Math.random().toString(36).substring(2, 15 + 2);
+const randomIdGenerator = () =>
+  Math.random()
+    .toString(36)
+    .substring(2, 15 + 2);
 
 // Decode token to get user ID
 const decodeToken = (token) => {
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        return decoded.userId;
-    } catch (error) {
-        console.error('Token decoding failed:', error);
-        return null;
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded.userId;
+  } catch (error) {
+    console.error("Token decoding failed:", error);
+    return null;
+  }
 };
 
 // Connexion de l'utilisateur au Websocket
 wss.on("connection", (ws, req) => {
-    const params = new URLSearchParams(req.url.split('?')[1]);
-    const token = params.get('token');
-    const userId = decodeToken(token);
+  const params = new URLSearchParams(req.url.split("?")[1]);
+  const token = params.get("token");
+  const userId = decodeToken(token);
 
-    if (!userId) {
-        ws.close();
-        return;
+  if (!userId) {
+    ws.close();
+    return;
+  }
+
+  // Si un joueur est déjà en attente, créer une partie
+  if (waitingPlayer) {
+    // Initialiser la partie
+    let gameId = randomIdGenerator();
+
+    games[gameId] = {
+      wsUserOne: {
+        id: userId,
+        ws: ws,
+      },
+      wsUserTwo: {
+        id: waitingPlayer.userId,
+        ws: waitingPlayer.ws,
+      },
+      messages: [],
+    };
+
+    // Envoyer le message de début de partie aux deux joueurs
+    ws.send(
+      JSON.stringify({
+        type: "start_game",
+        isFirst: true,
+        gameId: gameId,
+        userId: userId,
+      })
+    );
+
+    waitingPlayer.ws.send(
+      JSON.stringify({
+        type: "start_game",
+        isFirst: false,
+        gameId: gameId,
+        userId: waitingPlayer.userId,
+      })
+    );
+
+    // Enlever le joueur en attente
+    waitingPlayer = null;
+  } else {
+    // Mettre en attente le joueur si aucun joueur n'est en attente
+    waitingPlayer = {
+      ws: ws,
+      userId: userId,
+    };
+    ws.send(
+      JSON.stringify({
+        type: "waiting",
+      })
+    );
+  }
+
+  // Si aucun joueur ne s'est connecté en 5 secondes, créer une partie avec un bot
+  setTimeout(() => {
+    // Si le joueur est encore en attente, créer une partie
+    if (waitingPlayer && waitingPlayer.ws === ws) {
+      // Initialisation de la partie
+      let gameId = randomIdGenerator();
+      games[gameId] = {
+        wsUserOne: {
+          ws: ws,
+          id: userId,
+        },
+        wsUserTwo: null,
+        messages: [],
+        id: waitingPlayer.userId,
+      };
+
+      // Envoyer le message de début de partie au joueur
+      ws.send(
+        JSON.stringify({
+          type: "start_game",
+          gameId: gameId,
+          userId: userId,
+          againstBot: true,
+        })
+      );
+      waitingPlayer = null;
+    }
+  }, 5000);
+
+  // Ecoute sur les messages
+  ws.on("message", async (message) => {
+    // Formatage du message
+    message = JSON.parse(message);
+
+    // Vérifier si la partie existe, si non, quitter
+    let game = games[message.gameId];
+    if (!game) return;
+
+    // Ajout du message dans la partie
+    game.messages.push(message);
+
+    // Vérifier si la partie est terminée
+    let isGameOver = game.messages.length === 10 ? true : false;
+
+    // Si le message provient d'un être humain
+    if (message.type === "message_human") {
+      // Envoyer le message aux joueurs
+      game.wsUserOne.ws.send(
+        JSON.stringify({
+          type: "message_human",
+          content: message,
+          userId: game.userId,
+          isOver: isGameOver,
+        })
+      );
+      game.wsUserTwo.ws.send(
+        JSON.stringify({
+          type: "message_human",
+          content: message,
+          userId: game.userId,
+          isOver: isGameOver,
+        })
+      );
+    } else if (message.type === "message_bot") {
+      try {
+        setTimeout(async () => {
+          // Générer le message avec Ollama
+          let response = await ollama.chat({
+            model: process.env.OLLAMA_MODEL,
+            messages: [
+              ...game.messages,
+              {
+                role: "user",
+                content: message.content,
+              },
+            ],
+          });
+
+          // Formatage du message de Ollama
+          let res = response.message.content;
+          res = res.replace(/<think>.*?<\/think>/gs, "");
+
+          // Vérifier si la partie est terminée
+          const isGameOver = game.messages.length === 10 ? true : false;
+
+          // Envoyer le message aux joueurs
+          game.wsUserOne.ws.send(
+            JSON.stringify({
+              type: "message_bot",
+              content: {
+                content: res,
+                gameId: message.gameId,
+                type: "message_bot",
+                userId: "bot",
+              },
+              isOver: isGameOver,
+            })
+          );
+        }, 100);
+      } catch (error) {
+        console.error("Error parsing message:", error);
+      }
     }
 
-    // Si un joueur est déjà en attente, créer une partie
-    if (waitingPlayer) {
-        // Initialiser la partie
-        let gameId = randomIdGenerator();
-
-        games[gameId] = {
-            wsUserOne: {
-                id: userId,
-                ws: ws,
-            },
-            wsUserTwo: {
-                id: waitingPlayer.userId,
-                ws: waitingPlayer.ws,
-            },
-            messages: []
+    // At the end of the game, save messages and the game
+    if (isGameOver) {
+      const prepareMessages = game.messages.map((msg) => {
+        return {
+          content: msg.content,
+          bot: msg.userId === "bot" ? true : false,
+          user: msg.userId ? msg.userId : null,
         };
+      });
 
-        // Envoyer le message de début de partie aux deux joueurs
-        ws.send(JSON.stringify({
-            type: "start_game",
-            isFirst: true,
-            gameId: gameId,
-			userId: userId
-        }));
+      const messagesS = await Message.insertMany(prepareMessages);
 
-        waitingPlayer.ws.send(JSON.stringify({
-            type: "start_game",
-            isFirst: false,
-            gameId: gameId,
-            userId: waitingPlayer.userId
-        }));
+      let gameInfo = games[message.gameId];
+      console.log("GAME INFO", gameInfo);
+      console.log("USER ONE ID", gameInfo.wsUserOne.id);
+      console.log(
+        "USER TWO ID",
+        gameInfo.wsUserTwo ? gameInfo.wsUserTwo.id : null
+      );
 
-        // Enlever le joueur en attente
-        waitingPlayer = null;
-    } else {
+      const gameS = await Game.create({
+        gameId: message.gameId,
+        players: [
+          {
+            id: gameInfo.wsUserOne.id,
+            vote: null,
+          },
+          {
+            id: gameInfo.wsUserTwo ? gameInfo.wsUserTwo.id : null,
+            vote: null,
+          },
+        ],
+        messages: messagesS,
+        hadBot: gameInfo.wsUserTwo ? false : true,
+      });
 
-        // Mettre en attente le joueur si aucun joueur n'est en attente
-        waitingPlayer = {
-			ws: ws,
-			userId: userId
-		};
-        ws.send(JSON.stringify({
-            type: "waiting"
-        }));
+      gameS.save();
+      delete games[message.gameId];
     }
-
-    // Si aucun joueur ne s'est connecté en 5 secondes, créer une partie avec un bot
-    setTimeout(() => {
-        // Si le joueur est encore en attente, créer une partie
-        if (waitingPlayer && waitingPlayer.ws === ws) {
-            // Initialisation de la partie
-            let gameId = randomIdGenerator();
-            games[gameId] = {
-                wsUserOne: {
-                    ws: ws,
-                    id: userId
-                },
-                wsUserTwo: null,
-                messages: [],
-                id: waitingPlayer.userId
-            };
-
-            // Envoyer le message de début de partie au joueur
-            ws.send(JSON.stringify({
-                type: "start_game",
-                gameId: gameId,
-                userId: userId,
-                againstBot: true
-            }));
-            waitingPlayer = null;
-        }
-    }, 5000);
-
-    // Ecoute sur les messages
-    ws.on("message", async (message) => {
-        // Formatage du message
-        message = JSON.parse(message);
-
-        // Vérifier si la partie existe, si non, quitter
-        let game = games[message.gameId];
-        if (!game) return;
-
-        // Ajout du message dans la partie
-        game.messages.push(message);
-
-        // Vérifier si la partie est terminée
-        let isGameOver = game.messages.length === 5 ? true : false;
-
-        // Si le message provient d'un être humain
-        if (message.type === "message_human") {
-            // Envoyer le message aux joueurs
-            game.wsUserOne.ws.send(JSON.stringify({
-                type: "message_human",
-                content: message,
-                userId: game.userId,
-                isOver: isGameOver
-            }));
-            game.wsUserTwo.ws.send(JSON.stringify({
-                type: "message_human",
-                content: message,
-                userId: game.userId,
-                isOver: isGameOver
-            }));
-
-        } else if (message.type === "message_bot") {
-            try {
-				setTimeout(async () => {
-
-					// Générer le message avec Ollama
-					let response = await ollama.chat({
-						model: "deepseek-r1",
-						messages: [
-							...game.messages,
-							{
-								role: "user",
-								content: message.content
-							}
-						]
-					})
-
-					// Formatage du message de Ollama
-					let res = response.message.content;
-					res = res.replace(/<think>.*?<\/think>/gs, "");
-
-					// Vérifier si la partie est terminée
-					const isGameOver = game.messages.length === 5 ? true : false;
-
-					// Envoyer le message aux joueurs
-					game.wsUserOne.ws.send(JSON.stringify({
-						type: "message_bot",
-						content: {
-							content: res,
-							gameId: message.gameId,
-							type: "message_bot",
-							userId: "bot",
-						},
-						isOver: isGameOver
-					}));
-				}, 100)
-            } catch (error) {
-                console.error("Error parsing message:", error);
-            }
-        }
-
-        // At the end of the game, save messages and the game
-        if (isGameOver) {
-			
-			const prepareMessages = game.messages.map((msg) => {
-				return {
-					content: msg.content,
-					bot: msg.userId === "bot" ? true : false,
-					user: msg.userId ? msg.userId : null,
-				}
-			})
-
-			const messagesS = await Message.insertMany(prepareMessages);
-
-			let gameInfo = games[message.gameId];
-            console.log("GAME INFO", gameInfo);
-            console.log("USER ONE ID", gameInfo.wsUserOne.id);
-            console.log("USER TWO ID", gameInfo.wsUserTwo ? gameInfo.wsUserTwo.id : null);
-            
-			const gameS = await Game.create({
-				gameId: message.gameId,
-				players: [
-                    {
-                        id: gameInfo.wsUserOne.id,
-                        vote: null
-                    }, 
-                    {
-                        id: gameInfo.wsUserTwo ? gameInfo.wsUserTwo.id : null,
-                        vote: null
-                    }
-                ],
-				messages: messagesS,
-				hadBot: gameInfo.wsUserTwo ? false : true
-			})
-
-			gameS.save();
-			delete games[message.gameId];
-        }
-    })
+  });
 });
 
 const app = express();
@@ -234,14 +250,16 @@ app.use("/game", gameRoute);
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB connecté"))
-    .catch(err => console.log(err));
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connecté"))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Serveur lance sur le port " + PORT);
+  console.log("Serveur lance sur le port " + PORT);
 });
